f18aa4feff5a1b4446829afb89608f2b0cbd3075
JoseMarin <jmarinmalagon@gamil.com>
2021-04-30 14:02:29 +0200

    Update test01.js

--- test/test01.js
+++ test/test01.js
@@ -1,4 +1,20 @@
 function geekshubs(index) {
-    return "";
+    var msn = '';
+
+    for (let i = 1; i <= N; i++) {
+        
+      if (i%3==0 && i%5==0) {
+            msn = msn + "GeeksHubs\n";
+
+        } else if (i%3==0) {
+            msn = msn + "Geeks\n";
+
+        } else if (i%5==0) {
+            msn = msn + "Hubs\n"
+        } else {
+            msn += i+"\n"
+        }
+    }
+    return msn;
 }
 module.exports = geekshubs;
