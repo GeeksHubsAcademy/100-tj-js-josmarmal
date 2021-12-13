f0b98706c2cd957ca360aefc6a5c9fb07ed86c5e
Danrodsf <90701015+Danrodsf@users.noreply.github.com>
2021-12-13 16:55:53 +0100

    Primer test de funcion

--- test/test01.js
+++ test/test01.js
@@ -1,4 +1,15 @@
 function geekshubs(index) {
-    return "";
+  for (let i = 0; i <= index; i++) {
+    let string = "";
+    if (i % 3 == 0) {
+      string += "Geeks\n";
+    } else if (i % 5 == 0) {
+      string += "Hubs\n";
+    } else if (i % 3 == 0 && i % 5 == 0) {
+      string += "GeeksHubs\n";
+    } else {
+      string += `${i}\n`;
+    }
+  }
 }
 module.exports = geekshubs;
