74d18ce3f17ee5f168eb5145ba95018fcc54af82
Arnold <arnold201991@gmail.com>
2021-12-14 10:01:39 +0100

    delete code

--- test/test01.js
+++ test/test01.js
@@ -1,21 +1,4 @@
 function geekshubs(index) {
-    for (let i = 1; i <= index; i++) {
-
-        if (num % 3 == o) {
-
-            return "Geeks";
-
-
-        }
-        else if (index % 5 == 0) {
-
-            return "Hubs";
-
-        } else if ((num % 3 == 0) && (num % 5 == 0)) {
-            return "GeeksHubs";
-        } else {
-            return i;
-        }
-    }
+    return "";
 }
 module.exports = geekshubs;
