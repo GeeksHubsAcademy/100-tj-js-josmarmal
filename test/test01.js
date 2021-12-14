1190b02b53c034a2686791305a7801e54432cbd3
tamara96mc <tamara.96mc@gmail.com>
2021-12-14 10:05:58 +0100

    cambio en el for

--- test/test01.js
+++ test/test01.js
@@ -2,15 +2,15 @@ function geekshubs(index) {
 
     for (let i = 1; i <= index; i++) {
 
-        if (num % 3 == 0) {
+        if (i % 3 == 0) {
 
             return "Geeks";
 
-        } else if (index % 5 == 0) {
+        } else if (i % 5 == 0) {
 
             return "Hubs";
 
-        } else if ((num % 3 == 0)&&(num % 5 == 0)){
+        } else if ((i % 3 == 0)&&(i % 5 == 0)){
 
             return "GeeksHubs";
         } else {
@@ -18,6 +18,5 @@ function geekshubs(index) {
         }
 
     }
-
 }
 module.exports = geekshubs;
