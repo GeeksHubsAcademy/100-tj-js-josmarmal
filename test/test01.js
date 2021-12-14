a6230e7f5ce4717078f8f1b26202a2f83aa43a61
LordHob <rafaelginergomez@gmail.com>
2021-12-14 10:05:26 +0100

    prueba2

--- test/test01.js
+++ test/test01.js
@@ -1,7 +1,7 @@
 function geekshubs(index) {
     for (let i = 1; i <= index; i++) {
 
-        if (num % 3 == 0) {
+        if (index % 3 == 0) {
             return "Geeks";
 
         } else if (index % 5 == 0) {
