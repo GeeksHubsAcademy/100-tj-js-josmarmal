4660bc265d73755fd8e8517cc8126d9d32a0590d
Danrodsf <90701015+Danrodsf@users.noreply.github.com>
2021-12-14 09:57:55 +0100

    Cuarto intento

--- test/test01.js
+++ test/test01.js
@@ -3,17 +3,14 @@ function geekshubs(index) {
   for (let i = 1; i <= index; i++) {
     if (i % 3 == 0 && i % 5 == 0) {
       string += "GeeksHubs\n";
-    }
-    if (i % 5 == 0) {
+    } else if (i % 5 == 0) {
       string += "Hubs\n";
-    }
-    if (i % 3 == 0) {
+    } else if (i % 3 == 0) {
       string += "Geeks\n";
     } else {
       string += `${i}\n`;
     }
   }
-
   return string;
 }
 module.exports = geekshubs;
