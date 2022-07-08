3cc9b6fba29090a890c0f3b1123ff7fbdf372f4e
“AntonioML-sc” <antoniomoralesledesma@gmail.com>
2022-07-08 13:44:43 +0200

    Ej100 tests passed

--- test/test01.js
+++ test/test01.js
@@ -1,4 +1,29 @@
 function geekshubs(index) {
-    return "";
+
+    const multiplo = (num, div) => {
+        if (div == 0) {
+            return false
+        } else {
+            return (num % div == 0)
+        }
+    }
+
+    const makeString = (num) => {
+        let st = "";
+        for (let i = 1; i <= num; i++) {
+            if ((multiplo(i, 3)) && (multiplo(i, 5))) {
+                st += "GeeksHubs\n";
+            } else if (multiplo(i, 3)) {
+                st += "Geeks\n"
+            } else if (multiplo(i, 5)) {
+                st += "Hubs\n"
+            } else {
+                st += `${i}\n`
+            }
+        }
+        return st
+    }
+
+    return makeString(index);
 }
 module.exports = geekshubs;
